c8027c435dd73c588966e8c21de9fbebc3f45a67
MIGUELANGEL GARAY <miguelangelcdm@gmail.com>
2023-05-24 19:52:26 -0500

    front1

--- view/detalle/detalle.php
+++ view/detalle/detalle.php
@@ -1,8 +1,6 @@
-<link rel="stylesheet" href="assets/Curso/curso.css"  type="text/css">
-<h1>CURSOS</h1>
-<br>
-<select name="" id=""></select>
-<div class="container d-flex justify-content-center align-items-center ">
+<!-- <link rel="stylesheet" href="assets/Curso/curso.css"  type="text/css"> -->
+<!-- <select name="" id=""></select> -->
+<!-- <div class="container d-flex justify-content-center align-items-center ">
     <table class="table">
   <thead>
     <tr>
@@ -32,4 +30,62 @@
     </tr>
   </tbody>
 </table>
-</div>
+</div> -->
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar layout-without-menu">
+      <div class="layout-container" style="min-height:87vh">
+        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Content wrapper -->
+          <div class="content-wrapper">
+            <!-- Content -->
+            <div class="container-xxl flex-grow-1 container-p-y"style="max-width:100%">
+              <!-- Layout Demo -->
+              <div class="layout-demo-wrapper" style="height:100%">
+                <div class="card card-body" style="height:100%">
+
+                </div>
+              </div>
+              <!--/ Layout Demo -->
+            </div>
+            <!-- / Content -->
+
+            <div class="content-backdrop fade"></div>
+          </div>
+          <!-- Content wrapper -->
+        </div>
+        <!-- / Layout page -->
+      </div>
+    </div>
+    <!-- / Layout wrapper -->
+
+    <!-- <div class="buy-now">
+      <a
+        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
+        target="_blank"
+        class="btn btn-danger btn-buy-now"
+        >Upgrade to Pro</a
+      >
+    </div> -->
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="../assets/js/main.js"></script>
+
+    <!-- Page JS -->
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
--- view/header/header.php
+++ view/header/header.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="assets/Header/header.css"  type="text/css">
+<!-- <link rel="stylesheet" href="assets/Header/header.css"  type="text/css">
 <div class="container d-flex justify-content-center align-items-center ">
   <ul class="nav">
     <li class="nav-item">
@@ -11,5 +11,25 @@
       <a class="nav-link" href="logout">Cerrar sesion</a> 
     </li>
   </ul>
-</div>
+</div> -->
+<!-- Navbar -->
+<nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+    id="layout-navbar" style="max-width:100%">
+    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
 
+        <div class="navbar-nav align-items-center" style="width:100%; justify-content:space-between; flex-direction:row">
+            <div class="nav-item d-flex align-items-center">
+                <!-- <i class="bx bx-search fs-4 lh-0"></i>
+                <input type="text" class="form-control border-0 shadow-none" placeholder="Search..."
+                    aria-label="Search..." /> -->
+                    <img src="../assets/img/elements/usil.png" alt="" style="max-width:50px; margin-right:0.5rem">
+                    <a class="navbar-brand" href="javascript:void(0)">Sistema de Pseudomatricula</a>
+            </div>
+            <div class="nav-item d-flex align-items-center">
+              Bienvenido, Usuario:
+            </div>
+        </div>
+    </div>
+</nav>
+
+<!-- / Navbar -->
